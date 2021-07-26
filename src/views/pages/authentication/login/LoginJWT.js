eeb056deffdc19dd19bc901606e94340cf076daf
kingbal12 <kingbal999@gmail.com>
2021-07-26 17:53:24 +0900

    git commit -m

--- src/views/pages/authentication/login/LoginJWT.js
+++ src/views/pages/authentication/login/LoginJWT.js
@@ -37,38 +37,38 @@ class LoginJWT extends React.Component {
 
 
 
-  componentDidMount() {
-    if (!firebase.apps.length) {
+  // componentDidMount() {
+  //   if (!firebase.apps.length) {
 
-      firebase.initializeApp(config);  
-      const messaging = firebase.messaging();
+  //     firebase.initializeApp(config);  
+  //     const messaging = firebase.messaging();
 
-      messaging.usePublicVapidKey("BL0eTL3wIbAxmATwORsjQ-pNPCQBYrFNofCAr1xnArzbBjkRDreJLmiXYd-ySpazU-GTEAhtThWIhCLxYLvTGvY");
+  //     messaging.usePublicVapidKey("BL0eTL3wIbAxmATwORsjQ-pNPCQBYrFNofCAr1xnArzbBjkRDreJLmiXYd-ySpazU-GTEAhtThWIhCLxYLvTGvY");
 
-      //허가를 요청합니다!
-      Notification.requestPermission()
-      .then(function() {
-        console.log('허가!');
-        return messaging.getToken();
-      })
+  //     //허가를 요청합니다!
+  //     Notification.requestPermission()
+  //     .then(function() {
+  //       console.log('허가!');
+  //       return messaging.getToken();
+  //     })
 
-      .then(token => {
-        console.log(token); //토큰을 출력!
-        this.setState({tokendata:token})
-      })
+  //     .then(token => {
+  //       console.log(token); //토큰을 출력!
+  //       this.setState({tokendata:token})
+  //     })
 
-      .catch(function(err) {
-        console.log('fcm에러 : ', err);
-      })
+  //     .catch(function(err) {
+  //       console.log('fcm에러 : ', err);
+  //     })
     
-    } else {
+  //   } else {
     
-      firebase.app();
+  //     firebase.app();
     
-    }
+  //   }
 
     
-  }
+  // }
   
   
   
