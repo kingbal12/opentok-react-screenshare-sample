196972b191c336685a9dfe8a4a40b20d59f44bf6
kingbal12 <kingbal999@gmail.com>
2021-08-31 17:55:26 +0900

    git commit -m

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js'); 
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js'); 
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
 console.log("sw.js 실행됨")
 // Initialize Firebase 
 var config = { 
